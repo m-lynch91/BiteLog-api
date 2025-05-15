a7a7770ed9e5df1f849ad1b2df049d44b8df268d
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-15 18:56:39 -0400

    application properties and main

---
+++ src/main/java/org/milycorp/bitelog/BiteLogApplication.java
@@ -0,0 +1,13 @@
+package org.milycorp.bitelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiteLogApplication.class, args);
+    }
+
+}
