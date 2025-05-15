722631f25c7dcf4e09bd32d0a76f57c3ff8644ab
Mike Lynch <micheal.j.lynch@gmail.com>
2025-05-15 18:56:52 -0400

    application main test

---
+++ src/test/java/org/milycorp/bitelog/BiteLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.milycorp.bitelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
